8f35ee420cf2b80f8a8443b41ae87ea2b81d0637
munir79 <jakirhossainmunir79@gmail.com>
2025-04-25 22:09:24 +0600

    send Response middleware

--- src/modules/students/student.controllers.ts
+++ src/modules/students/student.controllers.ts
@@ -1,27 +1,55 @@
 //note controllers handle request and response
 //from client we can send data many ways like by params , by queries,and all
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { StudentServices } from "./student.service";
+import sendResponse from "../../app/utils/sendResponse";
+import  HttpStatus  from "http-status";
 
 
 
 
 //--------------------------- get all student controllers----------------------------------------
-const getAllStudentControllers = async (req: Request, res: Response) => {
+
+//******************************** */ without first error handeling error globally*********************************
+// const getAllStudentControllers = async (req: Request, res: Response) => {
+//   try {
+//     const result = await StudentServices.getAllStudentFromDB();
+//     res.status(200).json({
+//       sucess: true,
+//       message: "get all student sucessfully",
+//       data: result,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+
+
+// ********************************error handeling with globally********************************** 
+const getAllStudentControllers = async (req: Request, res: Response,next:NextFunction) => {
   try {
     const result = await StudentServices.getAllStudentFromDB();
-    res.status(200).json({
-      sucess: true,
-      message: "get all student sucessfully",
-      data: result,
-    });
+    // res.status(200).json({
+    //   sucess: true,
+    //   message: "get all student sucessfully",
+    //   data: result,
+    // });
+
+    sendResponse(res,{
+      statusCode:HttpStatus.OK,
+      sucess:true,
+      message:"get all student SucessFully",
+      data:result
+    })
   } catch (err) {
-    console.log(err);
+    // console.log(err);
+    next(err);
   }
 };
 
 // ------------------------------------- geta singleStudent from db--------------------------------
-const getASingleStudentControllers = async (req: Request, res: Response) => {
+const getASingleStudentControllers = async (req: Request, res: Response,next:NextFunction) => {
   try {
     const { studentId } = req.params;
     const result = await StudentServices.getASingleStudentFromDb(studentId);
@@ -31,7 +59,8 @@ const getASingleStudentControllers = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (err) {
-    console.log(err);
+    // console.log(err);
+    next(err)
   }
 };
 
